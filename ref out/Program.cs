0221834acb50c0562be95b5d3ebc3e9d3ee19865
Seymur03082001 <116655341+Seymur03082001@users.noreply.github.com>
2022-11-25 06:48:06 +0400

    first commit

---
+++ ref out/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace ref_out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1, 2,3};
+            AddLength(ref a, 4);
+            Console.WriteLine(a.Length);
+        }
+        static int[] InsertArray(int[] arr, int params);
+        int[] newArr = new int(list.Length + 1);
+         for (int i = 0; i<list.Length; i++)
+        {
+            newArr[i] = arr[i]
+        }
+    newArr[List.Length -1];
+    }
+return NewArr;
+}
